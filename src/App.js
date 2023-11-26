c0ed9721ae4c65107357f30a8255a8dc4d6cc7d9
Claudiu987 <andrei.domnita@student.usv.ro>
2023-11-26 17:18:36 +0200

    Forms

--- src/App.js
+++ src/App.js
@@ -1,31 +1,10 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import Home from "./pages/Home";
-import Menu from "./pages/Menu";
-import Contact from "./pages/Contact";
-
-import { createContext, useState } from "react";
-
-export const AppContext = createContext();
+import Form from "./pages/Form";
 
 function App() {
-  const [username, setUsername] = useState("Claudiu");
   return (
     <div className="App">
-      <AppContext.Provider value={{ username, setUsername }}>
-        <Router>
-          <div>
-            <Link to="/"> Home</Link>
-            <Link to="/menu"> Menu</Link>
-            <Link to="/contact"> Contact</Link>
-          </div>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/menu" element={<Menu />} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
-        </Router>
-      </AppContext.Provider>
+      <Form />
     </div>
   );
 }
---
+++ src/pages/Form.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+const Form = () => {
+  const schema = yup.object().shape({
+    fullName: yup.string().required(),
+    email: yup.string().required().email(),
+    age: yup.number().positive().integer().min(18).required(),
+    password: yup.string().min(4).max(20).required(),
+    confirmPassword: yup
+      .string()
+      .oneOf([yup.ref("password"), null])
+      .required(),
+  });
+
+  const { register, handleSubmit } = useForm({ resolver: yupResolver(schema) });
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input type="text" placeholder="Full name..." {...register("fullName")} />
+      <input type="text" placeholder="Email" {...register("email")} />
+      <input type="number" placeholder="Age" {...register("age")} />
+      <input
+        type="password"
+        placeholder="Password..."
+        {...register("password")}
+      />
+      <input
+        type="password"
+        placeholder="Confirm password..."
+        {...register("confirmPassword")}
+      />
+      <input type="submit"></input>
+    </form>
+  );
+};
+
+export default Form;
